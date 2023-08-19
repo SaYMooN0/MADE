691b662a40656304661871926dc186732cdb0e40
SaYMooN <shokoladnysaymoon@mail.ru>
2023-08-19 17:02:20 +0300

    Added a working tab system with style

--- MadeLib/wwwroot/js/pages/projectPage.js
+++ MadeLib/wwwroot/js/pages/projectPage.js
@@ -1,78 +1,74 @@
-
 $(function () {
-    alert("1");
-});
-//var project;
-//$(function () {
-//    alert("1");
-//    await setThemeOnStart();
-//    project = await invoke("get_current_project");
-//    console.log(project);
-//    if (project.loader == "forge") {
-//        if (["1.18.2", "1.16.5"].includes(project.version)) {
-//            addCustomMachineryBlock();
-//            addMasterfulMachineryBlock();
-//        }
-//        else if (project.version == "1.16.4") {
-//            addCustomMachineryBlock();
-//        }
-//    }
-//    else if (project.loader == "fabric") {
-//        if (project.version == "1.18.2") {
-//            addCustomMachineryBlock();
-//        }
-//    }
-//    addTab(welcomePage, "12")
-//    addTab("<p>123</p>", "18")
-//    switchToTab("welcome");
 
-//});
-//function addTab(content, name) {
-//    if (!name) {
-//        name = ($(".tabsContainer .tab-item").length + 1);
-//    }
-//    var newTabId = "tab-" + name;
-//    var tabItem = $("<li class='tab-item' data-tab='" + newTabId + "'><a href='#" + newTabId + "'>" + name + `</a>
-//    <svg class="tab-cross-btn" viewBox="0 0 24 24" fill="none">
-//        <path d="M16 8L8 16M8.00001 8L16 16" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-//    </svg></li>`);
-//    $(".tabsContainer").append(tabItem);
-//    var tabPage = $("<div class='tabPage' id='" + newTabId + "'>" + content + "</div>");
-//    $("#containerForPages").append(tabPage);
-//    $("#containerForPages").tabs("refresh");
-//    $("#sortable").sortable("refresh");
-//    let tabCrossBtn = tabItem.find(".tab-cross-btn");
-//    tabCrossBtn[0].addEventListener('click', closeTab);
-//}
-//function closeTab(event) {
-//    event.stopPropagation();
-//    let tabItem = $(this).closest('.tab-item');
-//    let tabId = tabItem.attr('data-tab');
-//    tabItem.remove();
-//    $("#" + tabId).remove();
-//    $("#containerForPages").tabs("refresh");
-//    $("#containerForPages").tabs("option", "active", 0);
-//}
-//function addWelcomeTab() {
-//    addTab(welcomePage, "welcome")
-//}
-//$(function () {
-//    $("#containerForPages").tabs();
-//    $("#sortable").sortable({
-//        axis: "x"
-//    });
-//    $("#sortable").disableSelection();
-//});
-//function switchToTab(tabName) {
-//    var newTabId = "tab-" + tabName;
-//    var tabIndex = $(".tabsContainer .tab-item[data-tab='" + newTabId + "']").index();
-//    $("#containerForPages").tabs("option", "active", tabIndex);
-//}
-//function addCustomMachineryBlock() {
-//    let machineBtnContainer = document.getElementById("machine-btn");
-//    machineBtnContainer.style.display = "grid";
-//}
-//function addMasterfulMachineryBlock() {
-//    let multiblockBtnContainer = document.getElementById("multiblock-btn");
-//    multiblockBtnContainer.style.display = "grid";
-//}
+    // Не создавайте вкладки с помощью jQuery UI в начале
+    // $("#containerForPages").tabs();
+
+    $("#sortable").sortable({
+        axis: "x"
+    });
+
+    $("#sortable").disableSelection();
+
+    addTab("<p>Content 1</p>", "1");
+    addTab("<h1>Content 2</h1>", "2");
+    addTab("<h4>Content 3</h4>", "3");
+
+    switchToTab("2");
+});
+
+function addTab(content, name) {
+    if (!name) {
+        name = ($("#sortable .tab-item").length + 1);
+    }
+    let newTabId = "tab-" + name;
+
+    let tabItem = $("<li class='tab-item' data-tab='" + newTabId + "'><label>" + name + `</label>
+    <svg class="tab-cross-btn" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#0F0F0F"/>
+</svg>
+    </li>`);
+
+    tabItem.click(function () {
+        switchToTab(name);
+    });
+
+    $("#sortable").append(tabItem);
+
+    let tabPage = $("<div class='tabPage' id='" + newTabId + "'>" + content + "</div>");
+
+    // Начнем с того, что все вкладки скрыты
+    tabPage.css('display', 'none');
+
+    $("#containerForPages").append(tabPage);
+
+    // Отключаем уничтожение и создание вкладок
+    // $("#containerForPages").tabs("destroy").tabs();
+    $("#sortable").sortable("refresh");
+
+    let tabCrossBtn = tabItem.find(".tab-cross-btn");
+    tabCrossBtn[0].addEventListener('click', closeTab);
+}
+
+function switchToTab(tabName) {
+    let newTabId = "tab-" + tabName;
+
+    // Сначала скрываем все вкладки
+    $(".tabPage").css('display', 'none');
+
+    // Затем отображаем нужную вкладку
+    $("#" + newTabId).css('display', 'block');
+}
+
+function closeTab(event) {
+    event.stopPropagation();
+    let tabItem = $(this).closest('.tab-item');
+    let tabId = tabItem.attr('data-tab');
+    tabItem.remove();
+    $("#" + tabId).remove();
+
+    // Отключаем уничтожение и создание вкладок
+    // $("#containerForPages").tabs("destroy").tabs();
+
+    // Переключимся на первую вкладку после удаления
+    switchToTab("1");
+}
