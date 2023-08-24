07914da6e99371e817ea42e672a2faa5683b49bd
SaYMooN <shokoladnysaymoon@mail.ru>
2023-08-24 23:06:55 +0300

    Finished the visual part for the main page of interaction with recipes

--- MadeLib/Src/TabContentClasses/ModifyRecipes.cs
+++ MadeLib/Src/TabContentClasses/ModifyRecipes.cs
@@ -7,24 +7,24 @@
            "<div class=\"action-buttons-container-5\">"+
 
 		   "<div id=\"newRecipeDiv\" class=\"action-button\">" +
-                "<label class=\"action-button-main-label\">Add vanilla recipe</label><br/>"+
-                "<label class=\"action-button-hint-label\">Create recipes for crafting table, furnace, stonecutter and so on </label>" +
+                "<label class=\"action-button-main-label\">Add vanilla recipe</label>"+
+                "<label class=\"action-button-hint-label\">Create recipes for crafting stations from vanilla minecraft </label>" +
             "</div>" +
            "<div id=\"deleteRecipeDiv\" class=\"action-button\">" +
-                "<label class=\"action-button-main-label\">Delete existing recipe</label><br/>" +
+                "<label class=\"action-button-main-label\">Delete recipe</label> " +
                 "<label class=\"action-button-hint-label\">Delete creation recipes for items and tags</label>" +
             "</div>" +
            "<div id=\"changeRecipeDiv\" class=\"action-button\">" +
-                "<label class=\"action-button-main-label\">Change existing recipe</label><br/>" +
+                "<label class=\"action-button-main-label\">Change recipe</label> " +
                 "<label class=\"action-button-hint-label\">Edit the recipes you have already created</label>" +
             "</div>" +
             "<div id=\"moddedRecipeDiv\" class=\"action-button\">" +
-                "<label class=\"action-button-main-label\">Add new modded recipe</label><br/>" +
-                "<label class=\"action-button-hint-label\">Create recipes for different mods that are supported by Made</label>" +
+                "<label class=\"action-button-main-label\">Add modded recipe</label> " +
+                "<label class=\"action-button-hint-label\">Create recipes for mods that are supported by Made</label>" +
             "</div>" +
            "<div id=\"groupRecipeDiv\" class=\"action-button\">" +
-                "<label class=\"action-button-main-label\">Add recipe group</label><br/>" +
-                "<label class=\"action-button-hint-label\">Create recipes for machines from mods in one go</label>" +
+                "<label class=\"action-button-main-label\">Add recipe group</label> " +
+                "<label class=\"action-button-hint-label\">Create recipes for machines from mods at a time</label>" +
             "</div>" +
 
            "</div>";
---
+++ MadeLib/Src/TabContentClasses/Welcome.cs
@@ -0,0 +1,10 @@
+﻿namespace MadeLib.Src.TabContentClasses
+{
+    abstract class Welcome
+    {
+        const string Style = "<link href=\"_content/MadeLib/css/pages/projectPage.css\" rel=\"stylesheet\" />";
+        const string Main = "";
+        const string Options = "";
+
+    }
+}
