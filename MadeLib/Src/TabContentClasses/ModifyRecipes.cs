f33b759d380948189e7b9a8dee9362507f4195f5
SaYMooN <shokoladnysaymoon@mail.ru>
2023-08-25 01:28:17 +0300

    Added script loading and minimal event handlers for the recipe interaction page

--- MadeLib/Src/TabContentClasses/ModifyRecipes.cs
+++ MadeLib/Src/TabContentClasses/ModifyRecipes.cs
@@ -4,25 +4,26 @@
     {
 		public const string Main =
            "<link href=\"_content/MadeLib/css/tab_content/components.css\" rel=\"stylesheet\" />" +
-           "<div class=\"action-buttons-container-5\">"+
+		   //"<script src=\"_content/MadeLib/js/tab_content/recipes.js\"></script>" +
+		   "<div class=\"action-buttons-container-5\">" +
 
-		   "<div id=\"newRecipeDiv\" class=\"action-button\">" +
+		   "<div id=\"newRecipeDiv\" class=\"action-button\" onclick=\"createActionOnClick()\">" +
                 "<label class=\"action-button-main-label\">Add vanilla recipe</label>"+
                 "<label class=\"action-button-hint-label\">Create recipes for crafting stations from vanilla minecraft </label>" +
             "</div>" +
-           "<div id=\"deleteRecipeDiv\" class=\"action-button\">" +
+		   "<div id=\"deleteRecipeDiv\" class=\"action-button\" onclick=\"deleteActionOnClick()\"> " +
                 "<label class=\"action-button-main-label\">Delete recipe</label> " +
                 "<label class=\"action-button-hint-label\">Delete creation recipes for items and tags</label>" +
             "</div>" +
-           "<div id=\"changeRecipeDiv\" class=\"action-button\">" +
+		   "<div id=\"changeRecipeDiv\" class=\"action-button\" onclick=\"changeActionOnClick()\">" +
                 "<label class=\"action-button-main-label\">Change recipe</label> " +
                 "<label class=\"action-button-hint-label\">Edit the recipes you have already created</label>" +
             "</div>" +
-            "<div id=\"moddedRecipeDiv\" class=\"action-button\">" +
+			"<div id=\"moddedRecipeDiv\" class=\"action-button\" onclick=\"moddedActionOnClick()\">" +
                 "<label class=\"action-button-main-label\">Add modded recipe</label> " +
                 "<label class=\"action-button-hint-label\">Create recipes for mods that are supported by Made</label>" +
             "</div>" +
-           "<div id=\"groupRecipeDiv\" class=\"action-button\">" +
+		   "<div id=\"groupRecipeDiv\" class=\"action-button\" onclick=\"groupActionOnClick()\">" +
                 "<label class=\"action-button-main-label\">Add recipe group</label> " +
                 "<label class=\"action-button-hint-label\">Create recipes for machines from mods at a time</label>" +
             "</div>" +
---
+++ MadeLib/wwwroot/js/tab_content/recipes.js
@@ -0,0 +1,5 @@
+function createActionOnClick() { alert("createActionOnClick");}
+function deleteActionOnClick() { alert("deleteActionOnClick");}
+function changeActionOnClick() { alert("changeActionOnClick");}
+function moddedActionOnClick() { alert("moddedActionOnClick");}
+function groupActionOnClick() { alert("groupActionOnClick");}
