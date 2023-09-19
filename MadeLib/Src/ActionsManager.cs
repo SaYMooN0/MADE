63060563f7329072ef7212caeb8d918f43927f04
SaYMooN <shokoladnysaymoon@mail.ru>
2023-09-19 15:42:17 +0300

    1. Deleting a letter on the crafting recipes creation page now also removes all its occurrences in the crafting grid 2.Added writing crafting table recipes in .js file

--- MadeLib/Src/ActionsManager.cs
+++ MadeLib/Src/ActionsManager.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using MadeLib.Src.ProjectClasses;
+using Newtonsoft.Json;
 
 namespace MadeLib.Src
 {
@@ -10,20 +11,19 @@ namespace MadeLib.Src
         static public HistoryItem HandleAction(ActionType actionType, Dictionary<string, string> arguments, string projectFolderPath)
         {
             string actionId = GenerateRecipeId();
-            return HandleAction(actionType,arguments,projectFolderPath,actionId);
+            return HandleAction(actionType, arguments, projectFolderPath, actionId);
         }
-        static public HistoryItem HandleAction(ActionType actionType, Dictionary<string, string> arguments, string projectFolderPath,string actionId)
+        static public HistoryItem HandleAction(ActionType actionType, Dictionary<string, string> arguments, string projectFolderPath, string actionId)
         {
             foreach (string key in arguments.Keys)
             {
-                arguments[key] = arguments[key].Replace("'",string.Empty);
+                arguments[key] = arguments[key].Replace("'", string.Empty);
             }
             switch (actionType)
             {
                 case ActionType.StonecutterAdd:
                     {
-                        string output = arguments["outputCount"] == "1" ? arguments["output"] : $"{arguments["outputCount"]}x {arguments["output"]}";
-                        string contentToWrite = $"event.stonecutting('{output}','{arguments["input"]}')";
+                        string contentToWrite = $"event.stonecutting('{returnItemWithCount(arguments["output"], arguments["outputCount"])}','{arguments["input"]}')";
                         JsFilesController.WriteVanillaRecipe(contentToWrite, projectFolderPath, actionId);
                         HistoryItem historyItem = new(arguments, JsFilesController.GetFullVanillaPath(), actionId, actionType);
                         return historyItem;
@@ -55,12 +55,49 @@ namespace MadeLib.Src
                         return historyItem;
 
                     }
+                case ActionType.CraftingTableAdd:
+                    {
+                        Dictionary<char, string> letterItemDictionary =
+                                JsonConvert.DeserializeObject<Dictionary<char, string>>(arguments["letterItemDictionary"]);
+                        if (letterItemDictionary.Count < 1) return null;        //in case suddenly the letterItemDictionary is empty
+
+                        Boolean.TryParse(arguments["isShapeless"], out bool isShapeless);
+                        string contentToWrite = "";
+                        if (isShapeless)
+                        {
+                            string itemsString = "";
+                            string? item;
+                            foreach (char letter in arguments["lettersInputGrid"]) {
+                                letterItemDictionary.TryGetValue(letter, out item);
+                                if(item!=null) itemsString += $"'{item}',";
+                                item = null;
+                            }
+                            itemsString = itemsString.Substring(0, itemsString.Length - 1);
+                            contentToWrite = $"event.shapeless('{returnItemWithCount(arguments["output"], arguments["outputCount"])}', [{itemsString}])";
+                        }
+                        else
+                        {
+                            string letterItemString = "";
+                            foreach (var letterItemPair in letterItemDictionary) { letterItemString += letterItemPair.Key + $": '{letterItemPair.Value}',"; }
+                            letterItemString = letterItemString.Substring(0, letterItemString.Length - 1);
+
+                            string[] gridInputStrings = arguments["lettersInputGrid"].Split(',');
+                            string gridInput = $"'{gridInputStrings[0]}'," +
+                                                  $"'{gridInputStrings[1]}'," +
+                                                  $"'{gridInputStrings[2]}'";
+                            contentToWrite = $" event.shaped('{returnItemWithCount(arguments["output"], arguments["outputCount"])}', [{gridInput}], {{{letterItemString}}})";
+                        }
+                        JsFilesController.WriteVanillaRecipe(contentToWrite, projectFolderPath, actionId);
+                        HistoryItem historyItem = new(arguments, JsFilesController.GetFullVanillaPath(), actionId, actionType);
+                        return historyItem;
+                    }
                 default: return null;
-            }  
+            }
         }
-        static public void DeleteAction(string ActionId, string fullPathToFile) { JsFilesController.DeleteAction(ActionId, fullPathToFile);  }
+        static public void DeleteAction(string ActionId, string fullPathToFile) { JsFilesController.DeleteAction(ActionId, fullPathToFile); }
         private static string GenerateRecipeId() => DateTime.Now.ToString("yyMMddHHmmss");
-       
+        private static string returnItemWithCount(string item, string itemCount) => itemCount == "1" ? item : $"{itemCount}x {item}";
+
 
     }
 }
--- MadeLib/wwwroot/js/tab_content/recipes.js
+++ MadeLib/wwwroot/js/tab_content/recipes.js
@@ -114,14 +114,14 @@ function craftingTableSaveButtonClick(e, isNew, actionId, path) {
     let isShapelessChecked = document.querySelector('[name="isShapeless"]').checked;
     let outputValue = document.querySelector(".crafting-table-output-div .default-input").value;
     let outputCountValue = document.querySelector(".crafting-table-output-div .default-input-num").value;
-    alert(JSON.stringify(letterItemDictionary));
     let arguments = {
-        ...letterItemDictionary,
-        lettersInput: gridValues.toString(),
+        letterItemDictionary: JSON.stringify(letterItemDictionary),
+        lettersInputGrid: gridValues.toString(),
         isShapeless: isShapelessChecked.toString(),
         output: outputValue,
         outputCount: outputCountValue
     };
+    
     if (isNew == "true") { addNewRecipeFromJS('CraftingTableAdd', arguments); }
     else if (isNew == "false") { changeExistingAction(actionId, path, 'CraftingTableAdd', arguments); }
     let submitButton = e.target.querySelector('.default-submit');
@@ -154,8 +154,16 @@ function addNewLetterForCraftingRecipe(event) {
     const removeButton = document.createElement('div');
     removeButton.classList.add('letter-delete-button-container');
     removeButton.innerHTML = `<svg class='letter-delete-button' viewBox='0 0 24 24' fill='none' > <path d='M20.5001 6H3.5' stroke='#1C274C' stroke-width='1.5' stroke-linecap='round'/> <path d='M18.8332 8.5L18.3732 15.3991C18.1962 18.054 18.1077 19.3815 17.2427 20.1907C16.3777 21 15.0473 21 12.3865 21H11.6132C8.95235 21 7.62195 21 6.75694 20.1907C5.89194 19.3815 5.80344 18.054 5.62644 15.3991L5.1665 8.5' stroke='#1C274C' stroke-width='1.5' stroke-linecap='round'/> <path d='M9.5 11L10 16' stroke='#1C274C' stroke-width='1.5' stroke-linecap='round'/> <path d='M14.5 11L14 16' stroke='#1C274C' stroke-width='1.5' stroke-linecap='round'/> <path d='M6.5 6C6.55588 6 6.58382 6 6.60915 5.99936C7.43259 5.97849 8.15902 5.45491 8.43922 4.68032C8.44784 4.65649 8.45667 4.62999 8.47434 4.57697L8.57143 4.28571C8.65431 4.03708 8.69575 3.91276 8.75071 3.8072C8.97001 3.38607 9.37574 3.09364 9.84461 3.01877C9.96213 3 10.0932 3 10.3553 3H13.6447C13.9068 3 14.0379 3 14.1554 3.01877C14.6243 3.09364 15.03 3.38607 15.2493 3.8072C15.3043 3.91276 15.3457 4.03708 15.4286 4.28571L15.5257 4.57697C15.5433 4.62992 15.5522 4.65651 15.5608 4.68032C15.841 5.45491 16.5674 5.97849 17.3909 5.99936C17.4162 6 17.4441 6 17.5 6' stroke='#1C274C' stroke-width='1.5'/></svg>`;
-    removeButton.addEventListener('click', function () { container.removeChild(newletterContainer); errorLabel.textContent = ''; });
-    const inputItem = newletterContainer.querySelector('.item-for-letter-input');
+    removeButton.addEventListener('click', function () {
+        const letterToRemove = newletterContainer.querySelector('.letter-label').innerText;
+        const gridItems = document.querySelectorAll('.crafting-table-grid-item');
+        gridItems.forEach(item => {
+            if (item.innerText === letterToRemove) { item.innerText = ''; }
+        });
+        container.removeChild(newletterContainer);
+        errorLabel.textContent = '';
+    });
+
     newletterContainer.setAttribute('draggable', 'true');
     newletterContainer.addEventListener('dragstart', handleDragStart);
     newletterContainer.appendChild(removeButton);
