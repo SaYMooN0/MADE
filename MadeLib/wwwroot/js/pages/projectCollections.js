37a2d9cbdf726f8463ef082bbacdc0cd30e6190c
SaYMooN <shokoladnysaymoon@mail.ru>
2023-09-25 17:10:21 +0300

    Continued working on the project collections page

---
+++ MadeLib/wwwroot/js/pages/projectCollections.js
@@ -0,0 +1,18 @@
+let tabs = document.querySelectorAll('.collection-tab');
+let contents = document.querySelectorAll('.tab-content');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', function () {
+        tabs.forEach(innerTab => innerTab.classList.remove('active'));
+        contents.forEach(content => content.style.display = 'none');
+        tab.classList.add('active');
+        let tabContent = document.querySelector(`.tab-content[data-content='${tab.getAttribute("data-tab")}']`);
+        if (tabContent) {
+            tabContent.style.display = 'block';
+        }
+    });
+});
+function collectionAddNewPressesd() {
+    let inputValue = document.querySelector('.add-new-input').value;
+    document.querySelector('.add-new-message').textContent = inputValue;
+}
