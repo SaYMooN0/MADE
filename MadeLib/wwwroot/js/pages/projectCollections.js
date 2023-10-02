9da9803922b958a40914346d773260d960ea2439
SaYMooN <shokoladnysaymoon@mail.ru>
2023-10-02 20:05:13 +0300

    added frontend functionality of the edit button for the items on the project collection page

--- MadeLib/wwwroot/js/pages/projectCollections.js
+++ MadeLib/wwwroot/js/pages/projectCollections.js
@@ -18,10 +18,66 @@ function collectionAddNewPressesd() {
 function collectionItemClicked(e) {
     alert(JSON.stringify(e));
 }
-function collectionItemEdit(e) {
-  
-    alert('edit');
-    e.stopPropagation();
+
+
+function collectionItemEdit(event) {
+    event.stopPropagation();
+    changeAllToLabels();
+
+    let item = event.target.parentElement;
+    let label = item.querySelector('.collection-item-label');
+    let input = item.querySelector('.collection-item-input');
+
+    input.value = label.textContent;
+
+    label.style.display = 'none';
+    item.querySelector('.collection-item-edit-button').style.display = 'none';
+    item.querySelector('.collection-item-delete-button').style.display = 'none';
+
+    input.style.display = 'block';
+    item.querySelector('.collection-item-save-button').style.display = 'block';
+    item.querySelector('.collection-item-cancel-button').style.display = 'block';
+}
+
+function collectionItemSave(event) {
+    event.stopPropagation();
+
+    let item = event.target.parentElement;
+    let label = item.querySelector('.collection-item-label');
+    let input = item.querySelector('.collection-item-input');
+
+    label.textContent = input.value;
+
+    input.style.display = 'none';
+    item.querySelector('.collection-item-save-button').style.display = 'none';
+    item.querySelector('.collection-item-cancel-button').style.display = 'none';
+
+    label.style.display = 'block';
+    item.querySelector('.collection-item-edit-button').style.display = 'block';
+    item.querySelector('.collection-item-delete-button').style.display = 'block';
+}
+
+function collectionItemCancel(event) {
+    event.stopPropagation();
+
+    let item = event.target.parentElement;
+    item.querySelector('.collection-item-input').style.display = 'none';
+    item.querySelector('.collection-item-save-button').style.display = 'none';
+    item.querySelector('.collection-item-cancel-button').style.display = 'none';
+
+    item.querySelector('.collection-item-label').style.display = 'block';
+    item.querySelector('.collection-item-edit-button').style.display = 'block';
+    item.querySelector('.collection-item-delete-button').style.display = 'block';
+}
+function changeAllToLabels() {
+    let items = document.querySelectorAll('.collection-item');
+
+    items.forEach(function (item) {
+        let cancelButton = item.querySelector('.collection-item-cancel-button');
+        if (cancelButton.style.display !== 'none') {
+            collectionItemCancel({ target: cancelButton, stopPropagation: function () { } });
+        }
+    });
 }
 function collectionItemDelete(e) {
  
